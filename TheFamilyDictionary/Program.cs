614a51c7a4ba6060fd5eaff5b6d410179ca3ed27
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-09-10 17:32:15 -0500

    finished family dictionary exercise

--- TheFamilyDictionary/Program.cs
+++ TheFamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TheFamilyDictionary
 {
@@ -6,7 +7,23 @@ namespace TheFamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("brother", new Dictionary<string, string>{
+                {"name","Kevin"},
+                {"age","8"}
+            });
+
+            myFamily.Add("cousin", new Dictionary<string, string>{
+                {"name","Paul"},
+                {"age","14"}
+            });
+
+            foreach (KeyValuePair<string, Dictionary<string, string>> member in myFamily)
+            {
+                // do something with entry.Value or entry.Key
+                Console.WriteLine("My name is " + member.Value["name"] + " and is " + member.Value["age"] + " years old!");
+            }
         }
     }
 }
